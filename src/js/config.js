c357ea23bd251f797c9ad3b0f56de624c28edca3
Andranik1133 <ando.aleksanyan93@gmail.com>
2024-11-22 16:46:55 +0400

    my project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_ESC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "dcca2993-c703-4849-b48b-789ba0ae3acb";
+export const MODAL_CLOSE_SEC = 2.5;
